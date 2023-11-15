f79dd888479792daa2e5fd2a097a35ca388cf3d6
BrunoPaniagua <brunopaniaguaoliveira@gmail.com>
2023-11-14 23:01:08 -0300

    Primeira versão do front

--- main.js
+++ main.js
@@ -5,13 +5,24 @@ let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 960,
-    height: 540,
+    width: 980,
+    height: 650,
+    // seta o tamanho minimo e maximo da aplicacao -> ultima alteracao feita por PANIAGUA
+    minHeight:650,
+    minWidth:980,
+    maxWidth:1920,
+    maxHeight:1080,
+    //----------------------------------------------------------------------------
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
+    
   });
+
+  // Define o ícone da aplicação -> ultima alteracao feita por PANIAGUA
+  mainWindow.setIcon("./img/icon.png");
+  //------------------------------------------------------------
 
   mainWindow.setMenu(null);
   mainWindow.loadFile('index.html');
