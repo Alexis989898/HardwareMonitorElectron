8c78242ca5bf2bf4d2840a767345570ed495e01b
Alexis989898 <alexisvitor6@gmail.com>
2023-11-22 00:57:46 -0300

    Add Cpu and Ram usage

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 980,
     height: 650,
-    // seta o tamanho minimo e maximo da aplicacao -> ultima alteracao feita por PANIAGUA
+    // seta o tamanho minimo e maximo da aplicacao
     minHeight: 650,
     minWidth: 980,
     maxWidth: 1920,
@@ -20,7 +20,7 @@ app.on('ready', () => {
 
   });
 
-  // Define o ícone da aplicação -> ultima alteracao feita por PANIAGUA
+  // Define o ícone da aplicação
   const path = require('path');
   const iconPath = path.join(__dirname, 'img', 'icon.png');
   mainWindow.setIcon(iconPath);
@@ -29,11 +29,16 @@ app.on('ready', () => {
   mainWindow.setMenu(null);
   mainWindow.loadFile('hardware.html');
 
+  let datasent = 0;
+
+
+  //Funções das infos básicas
   const CPUInfo = async () => {
     try {
       const cpuData = await si.cpu();
       const cpuBrand = cpuData.brand;
       const cpuManufacturer = cpuData.manufacturer;
+      datasent++;
       if (mainWindow) {
         mainWindow.webContents.send('cpu-info', cpuBrand, cpuManufacturer);
       }
@@ -48,6 +53,7 @@ app.on('ready', () => {
       const ramIndividualData = await si.memLayout();
       const ramType = ramIndividualData[0].type;
       const ramTotal = (ramData.total / (1024 * 1024 * 1024)).toFixed(2) + " GBs";
+      datasent++;
       if (mainWindow) {
         mainWindow.webContents.send('ram-info', ramType, ramTotal);
       }
@@ -59,8 +65,9 @@ app.on('ready', () => {
   const GPUInfo = async () => {
     try {
       const gpuData = await si.graphics();
-      const gpuName = gpuData.model;
-      const gpuManufacturer = gpuData.vendor;
+      const gpuName = gpuData.controllers[0].model;;
+      const gpuManufacturer = gpuData.controllers[0].vendor;
+      datasent++;
       if (mainWindow) {
         mainWindow.webContents.send('gpu-info', gpuName, gpuManufacturer);
       }
@@ -68,18 +75,51 @@ app.on('ready', () => {
       console.error('Error fetching GPU info:', error);
     }
   };
+  //------------------------------------------------------------
+
+  //Função da info de uso
+  const UsageInfo = async () => {
+    try {
+      const { currentLoad } = await si.currentLoad();
+      const { used, total } = await si.mem();
+      const ramUsage = (used / total) * 100;
+
+      mainWindow.webContents.send('UsageInfo', { cpuUsage: currentLoad, ramUsage});
+    } catch (error) {
+        console.error('Error fetching usage information:', error);
+    }
+  }
+
+  const GPUUsageInfo = async () => {
+    try {
+      const gpuData = await si.graphics();
+      const gpuUsage = gpuData.controllers[0].utilizationGpu;
+      mainWindow.webContents.send('GPUUsageInfo', { gpuUsage }); 
+    } catch (error) {
+      console.error('Error fetching GPU usage information:', error);
+    }
+  }
+  //------------------------------------------------------------
+
+  let intervalId;
+  function loadCheck() {
+    if (datasent < 3) {
+        intervalId = setInterval(loadCheck, 1000);
+    } else {
+        clearInterval(intervalId); // Clear the interval
+        mainWindow.webContents.send('data-loaded');
+    }
+  }
 
   ipcMain.on('infoRequest', (event) => {
-
     CPUInfo();
     RAMInfo();
     GPUInfo();
-    mainWindow.webContents.send('data-loaded');
+    loadCheck();
   });
 
-
-
-  //setInterval(sendRAMUsage, 1000);
+  setInterval(UsageInfo, 1200);
+  //setInterval(GPUUsageInfo, 1200);
 
   globalShortcut.register('CommandOrControl+Shift+I', () => {
     mainWindow.webContents.openDevTools();
--- renderer.js
+++ renderer.js
@@ -2,22 +2,19 @@ const { ipcRenderer } = require('electron');
 
 //Insere as divs em variáveis
 const div_cpu_name = document.getElementById('cpu-name');
-const div_cpu_name_previous = div_cpu_name.innerHTML;
-
 const div_cpu_manufacturer = document.getElementById('cpu-manufacturer');
-const div_cpu_manufacturer_previous = div_cpu_manufacturer.innerHTML;
-
 const div_ram_type = document.getElementById('ram-type');
-const div_ram_type_previous = div_ram_type.innerHTML;
-
 const div_ram_total = document.getElementById('ram-total');
-const div_ram_total_previous = div_ram_total.innerHTML;
-
 const div_gpu_name = document.getElementById('gpu-name');
-const div_gpu_name_previous = div_gpu_name.innerHTML;
-
 const div_gpu_manufacturer = document.getElementById('gpu-manufacturer');
-const div_gpu_manufacturer_previuos = div_gpu_manufacturer.innerHTML;
+
+
+const div_cpu_usage = document.getElementById('cpu-usage');
+const div_cpu_usage_text = div_cpu_usage.innerHTML;
+const div_ram_usage = document.getElementById('ram-usage');
+const div_ram_usage_text = div_ram_usage.innerHTML;
+const div_gpu_usage = document.getElementById('gpu-usage');
+const div_gpu_usage_text = div_gpu_usage.innerHTML;
 //------------------------------------------------------------
 
 
@@ -26,11 +23,11 @@ overlay.style.display = 'block';
 //Pede o envio de dados para o main.js
 ipcRenderer.send('infoRequest');
 
-//Recebe o conteúdo de main.js
+//Recebe o conteúdo de main.js e insere no html
 ipcRenderer.on('cpu-info', (event, cpuBrand, cpuManufacturer) => {
     if (cpuBrand && cpuManufacturer) {
-        const cpu_name = div_cpu_name_previous + cpuBrand;
-        const cpu_manu = div_cpu_manufacturer_previous + cpuManufacturer;
+        const cpu_name = div_cpu_name.innerHTML + cpuBrand;
+        const cpu_manu = div_cpu_manufacturer.innerHTML + cpuManufacturer;
 
         div_cpu_name.innerHTML = cpu_name;
         div_cpu_manufacturer.innerHTML = cpu_manu;
@@ -41,8 +38,8 @@ ipcRenderer.on('cpu-info', (event, cpuBrand, cpuManufacturer) => {
 
 ipcRenderer.on('ram-info', (event, ramType, ramTotal) => {
     if(ramType && ramTotal) {
-        const ram_type = div_ram_type_previous + ramType;
-        const ram_total = div_ram_total_previous + ramTotal;
+        const ram_type = div_ram_type.innerHTML + ramType;
+        const ram_total = div_ram_total.innerHTML + ramTotal;
 
         div_ram_type.innerHTML = ram_type;
         div_ram_total.innerHTML = ram_total;
@@ -51,25 +48,39 @@ ipcRenderer.on('ram-info', (event, ramType, ramTotal) => {
     }
 });
 
-ipcRenderer.on('gpu-info', (event, gpuType, ramTotal) => {
+ipcRenderer.on('gpu-info', (event, gpuType, gpuManufacturer) => {
+    if(gpuType && gpuManufacturer) {
+        const gpu_name =  div_gpu_name.innerHTML + gpuType;
+        const gpu_manufacturer = div_gpu_manufacturer.innerHTML + gpuManufacturer;
 
+        div_gpu_name.innerHTML = gpu_name;
+        div_gpu_manufacturer.innerHTML = gpu_manufacturer;
+    } else {
+        console.error('Invalid GPU data', gpuType, gpuManufacturer);
+    }
 });
 //------------------------------------------------------------
 
-ipcRenderer.on('data-loaded', () => {
-    overlay.style.display = 'none';
-});
-
-
-/*
-ipcRenderer.on('ram-usage', (event, ramUsage) => {
-    if (ramUsage && ramUsage.used && ramUsage.total) {
-        const usedMemory = ramUsage.used / 1024 / 1024;
-        const totalMemory = ramUsage.total / 1024 / 1024;
-        const usageString = `RAM Usage: ${usedMemory.toFixed(2)} MB / ${totalMemory.toFixed(2)} MB`;
-        ramUsageElement.innerText = usageString;
+ipcRenderer.on('UsageInfo', (event, { cpuUsage, ramUsage }) => {
+    if (cpuUsage !== undefined && ramUsage !== undefined) {
+        const cpuU = div_cpu_usage_text + cpuUsage.toFixed(2) + '%';
+        const ramU = div_ram_usage_text + ramUsage.toFixed(2) + '%';
+        div_cpu_usage.innerHTML = cpuU;
+        div_ram_usage.innerHTML = ramU;
     } else {
-        console.error('Invalid RAM usage data:', ramUsage);
+        console.error('Invalid usage data:', { cpuUsage, ramUsage });
     }
 });
-*/
+
+ipcRenderer.on('GPUUsageInfo', (event, { gpuUsage }) => { //Not Working
+    if (gpuUsage !== undefined) {
+        const gpuU = div_gpu_usage_text + gpuUsage.toFixed(2) + '%';
+        div_gpu_usage.innerHTML = gpuU;
+    } else {
+        console.error('Invalid GPU usage data:', { gpuUsage });
+    }
+}); 
+
+ipcRenderer.on('data-loaded', (event, datasent) => {
+    overlay.style.display = 'none';
+});
